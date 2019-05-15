0e5ab487b205729edada51cc93bedda02231199c
CERVANTES ROMERO JAVIER <jcervant23@cuc.edu.co>
2019-05-15 16:59:27 -0500

    Titulo modificado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel papu
+                   Jorge es mk
                 </div>
 
                 <div class="links">
